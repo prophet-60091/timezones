e877d00e266c81aa6885f217052c959b590bd34c
prophet-60091 <DustinJSanders@gmail.com>
2020-05-08 21:33:37 +0200

    Create awaretime.py

---
+++ awaretime.py
@@ -0,0 +1,29 @@
+import datetime
+import pytz
+
+'''
+https://stackoverflow.com/questions/13866926/is-there-a-list-of-pytz-timezones
+
+To get all possible timezones:
+
+for tz in pytz.all_timezones:
+    print tz
+'''
+
+fmt = '%d-%m %H:%M'
+UTC = datetime.datetime.now(tz=pytz.UTC)
+
+CET = UTC.astimezone(pytz.timezone('CET'))
+PDT = UTC.astimezone(pytz.timezone('US/Pacific'))
+CDT = UTC.astimezone(pytz.timezone('US/Central'))
+WAUS = UTC.astimezone(pytz.timezone('Australia/Perth'))
+NSWAUS = UTC.astimezone(pytz.timezone('Australia/Sydney'))
+QAUS = UTC.astimezone(pytz.timezone('Australia/Brisbane'))
+
+print('Perth ' + NSWAUS.strftime(fmt))
+print('Brisbane ' + QAUS.strftime(fmt))
+print('Sydney ' + WAUS.strftime(fmt))
+print('Berlin ' + CET.strftime(fmt))
+print('UTC ' + UTC.strftime(fmt))
+print('Dallas ' + CDT.strftime(fmt))
+print('Portland ' + PDT.strftime(fmt))
